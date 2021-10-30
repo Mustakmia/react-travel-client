9b8c50cd4eae1a5263aa53cc20da7e414450f98b
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-10-31 05:08:48 +0600

    main

---
+++ src/Firebase/config.js
@@ -0,0 +1,5 @@
+const firebaseConfig = {
+<h1>আপনার ফায়ারবেইজের ফাইলটি বসাবেন এখানে</h1>
+};
+
+export default firebaseConfig
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div>
+      <Container>
+        <h1>This is banner</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
