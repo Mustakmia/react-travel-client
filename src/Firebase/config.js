17a5b41c846ae37067a2cf7a3d82a77ea020511a
Mustakmia <musyafijur52@gmail.com>
2021-11-01 23:45:28 +0600

    final

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD9VBQRK9TM8ZpnM3etWTV0WiJ4WV6z7qE",
-    authDomain: "world-tour-b7fb6.firebaseapp.com",
-    projectId: "world-tour-b7fb6",
-    storageBucket: "world-tour-b7fb6.appspot.com",
-    messagingSenderId: "3856074309",
-    appId: "1:3856074309:web:e4635dc3442a3ccdb4052c",
-    measurementId: "G-JMHTR8BGVM"
+    apiKey: "AIzaSyCNNwzpYSvs3va8J5e5RAGU8s4xA4MhPuQ",
+    authDomain: "world-tour-561a1.firebaseapp.com",
+    projectId: "world-tour-561a1",
+    storageBucket: "world-tour-561a1.appspot.com",
+    messagingSenderId: "973888257690",
+    appId: "1:973888257690:web:37d5d3c00b425187b02923",
+    measurementId: "G-07PSTH7RL4"
 };
 
 export default firebaseConfig
